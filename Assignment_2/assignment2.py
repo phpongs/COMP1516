85b15df53bb1064421ab3da991f49033a3ebbbb4
meannnph <122788933+meannnph@users.noreply.github.com>
2024-03-06 21:27:37 -0800

    update assignment2

--- Assignment_2/assignment2.py
+++ Assignment_2/assignment2.py
@@ -5,58 +5,58 @@ import re
 
 
 def write_countries_capitals_to_file(filename):
-
-    filename_check = r"^[a-zA-Z\d]{1,8}\.txt$"
-    while not re.match(filename_check, filename):
+    while True:
         filename = input("Enter filename (1-8 letters or number, end with .txt): ")
-
-    else:
-        file = open(filename, "w")
-        for country, capital in countries_capitals_dictionary.items():
-            file.write(f"The capital of {country} is {capital}.\n")
-        file.close()
+        if re.search(r"^[a-zA-z0-9]{1,8}.txt$", filename):
+            file = open(filename, "w")
+            for country, capital in countries_capitals_dictionary.items():
+                file.write(f"The capital of {country} is {capital}.\n")
+            file.close()
+            break
+        else:
+            print("Try again, file name must contains 1-8 letters or number, end with .txt")
 
 
 def save_capitals():
 
     file = open("vowel_vowel_vowel.txt", "w")
     for capital in capitals:
-        if re.search(r"[aeiouAEIOU]{3}", capital):
+        if re.search(r"[aeiou]{3}", capital, re.IGNORECASE):
             file.write(f"{capital}\n")
     file.close()
 
     file = open("cons_cons_cons.txt", "w")
     for capital in capitals:
-        if re.search(r"[^aeiouAEIOU\s]{3}", capital):
+        if re.search(r"[bcdfghjklmnpqrstvwxyz]{3}", capital, re.IGNORECASE):
             file.write(f"{capital}\n")
     file.close()
 
     file = open("i_before_e.txt", "w")
     for capital in capitals:
-        if re.search(r"[iI].*[eE]", capital):
+        if re.search(r"i.*e", capital, re.IGNORECASE):
             file.write(f"{capital}\n")
     file.close()
 
     file = open("a_a.txt", "w")
     for capital in capitals:
-        if re.search(r"^[aA].*[aA]$", capital):
+        if re.search(r"^a.*a$", capital, re.IGNORECASE):
             file.write(f"{capital}\n")
     file.close()
 
     file = open("end_with_vowel.txt", "w")
     for capital in capitals:
-        if re.search(r"[aeiouAEIOU]$", capital):
+        if re.search(r"[aeiou]$", capital, re.IGNORECASE):
             file.write(f"{capital}\n")
     file.close()
 
     file = open("weird.txt", "w")
     for capital in capitals:
-        if re.search(r"[\sxX]", capital):
+        if re.search(r"['\sx]", capital, re.IGNORECASE):
             file.write(f"{capital}\n")
     file.close()
 
     file = open("not_start.txt", "w")
     for capital in capitals:
-        if not re.search(r"^[a-eA-Ep-sP-S]", capital):
+        if not re.search(r"^[a-ei-ps]", capital, re.IGNORECASE):
             file.write(f"{capital}\n")
     file.close()
